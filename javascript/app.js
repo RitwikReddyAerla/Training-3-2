8a3a36ef3abe923461cf79c252f63f6370d9e7aa
RitwikReddyAerla <ritwikreddyaerla185609@gmail.com>
2025-01-07 17:36:24 +0530

    updated files

--- javascript/app.js
+++ javascript/app.js
@@ -1,49 +1,49 @@
-// console.log("Js is linked to html....")
-// document.writeln("js linked")
+console.log("Js is linked to html....")
+document.writeln("js linked")
 
-// var user = "Sam"
-// var user = "Miller" //reinitialization  possible
-// console.log(user) 
+var user = "Sam"
+var user = "Miller" //reinitialization  possible
+console.log(user) 
 
-// let num = 1000
-// num = true //reinitialization  possible
-// console.log(num)
+let num = 1000
+num = true //reinitialization  possible
+console.log(num)
 
-// const dob = 2005
-// dob = 2000 //re-initalization is not possible
-// console.log(dob)
+const dob = 2005
+dob = 2000 //re-initalization is not possible
+console.log(dob)
 
-// var a = 10
-// var a = 30  //re-declaration is possible with var
-// console.log(a)
+var a = 10
+var a = 30  //re-declaration is possible with var
+console.log(a)
 
-// let b = 40;
-// // let b = 50;  let cannot be re-declared
-// console.log(b)
+let b = 40;
+// let b = 50;  let cannot be re-declared
+console.log(b)
 
-// let str1 = "double"
-// console.log(str1)
-// console.log(typeof(str1))
+let str1 = "double"
+console.log(str1)
+console.log(typeof(str1))
 
-// let str2 = 'single'
-// console.log(str2)
-// console.log(typeof(str2))
-
-
-// let str3 = `double`
-// console.log(str3)
-// console.log(typeof(str3))
-
-// let boo = true
-// console.log(boo)
-// console.log(typeof(boo))
+let str2 = 'single'
+console.log(str2)
+console.log(typeof(str2))
 
 
-// let a
-// console.log(a)
-// console.log(typeof(a))
+let str3 = `double`
+console.log(str3)
+console.log(typeof(str3))
+
+let boo = true
+console.log(boo)
+console.log(typeof(boo))
 
 
-// let nu = null
-// console.log(nu)
-// console.log(typeof(nu))
+let a
+console.log(a)
+console.log(typeof(a))
+
+
+let nu = null
+console.log(nu)
+console.log(typeof(nu))
